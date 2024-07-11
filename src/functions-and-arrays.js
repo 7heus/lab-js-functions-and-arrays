50a4ad3e3beea71931d1e2df36f43d031956632b
Theus <matheusgoias@hotmail.com>
2024-07-11 17:13:24 +0100

    Matheus WDFT JUL2024

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,78 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else {
+    return num1 || num2;
+  }
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
-
-
-
+function findLongestWord(array) {
+  if (array.length === 0) {
+    return null;
+  }
+  let word = "";
+  for (i = 0; i < array.length; i++) {
+    if (word.length < array[i].length) {
+      word = array[i];
+    }
+  }
+  return word;
+  //   return array.reduce((a, b) => (a.length < b.length ? b : a), "");
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(numArr) {
+  if (numArr.length === 0) {
+    return 0;
+  }
+  let numRe = 0;
+  numArr.forEach((num) => (numRe += num));
+  return numRe;
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
-
+function averageNumbers(numArr) {
+  if (numArr.length === 0) {
+    return 0;
+  }
+  let numRe = 0;
+  numArr.forEach((num) => (numRe += num));
+  return numRe / numArr.length;
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(arr, word) {
+  if (arr.length === 0) {
+    return null;
+  }
+  return arr.includes(word);
+}
